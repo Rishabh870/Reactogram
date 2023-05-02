2014bbb0ebe4f8c64ddd42f6f9c168cfed7704d1
Rishabh870 <67893503+Rishabh870@users.noreply.github.com>
2023-05-02 22:20:49 +0530

    Added Link to header

--- reactogram-fe/src/components/Navbar.jsx
+++ reactogram-fe/src/components/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import logo from '../images/logo.PNG';
 import { NavLink } from 'react-router-dom';
+import { Dropdown } from 'react-bootstrap';
+
 import './navbar.css';
 const Navbar = () => {
   return (
     <div className='nav-shadow'>
       <nav className='navbar navbar-light bg-light '>
         <nav className='container-fluid'>
-          <a className='navbar-brand m-0 ms-md-5'>
+          <NavLink to='/' className='navbar-brand m-0 ms-md-5'>
             <img src={logo} alt='logo' height='45px' />
-          </a>
+          </NavLink>
           <form className='d-flex me-md-5' role='search'>
             <input
               className='form-control me-3 d-none d-md-block text-muted searchbox'
@@ -27,9 +29,40 @@ const Navbar = () => {
             <a className='nav-link fs-5 mx-3' href='#'>
               <i className='fa-regular fa-heart '></i>
             </a>
-            <NavLink to='/myprofile' className='nav-link fs-5 mx-3' href='#'>
-              <i className='fa-solid fa-circle-user'></i>
-            </NavLink>
+            <Dropdown className=''>
+              <Dropdown.Toggle
+                variant='transparent'
+                id='dropdown-basic'
+                toggleIcon={false}
+                style={{ marginTop: '-4px' }}
+              >
+                <img
+                  src='https://images.unsplash.com/photo-1681967712041-9f274b3ee850?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1075&q=80'
+                  alt='Profile'
+                  className='rounded-circle me-2 float-start'
+                  width='25px'
+                  height='25px'
+                />
+              </Dropdown.Toggle>
+
+              <Dropdown.Menu>
+                <Dropdown.Item>
+                  <NavLink
+                    to='/myprofile'
+                    className='px-2 py-0 text-decoration-none text-dark'
+                    href='#'
+                  >
+                    <i className='fa-solid fa-circle-user'></i> My Profile
+                  </NavLink>
+                </Dropdown.Item>
+                <Dropdown.Item>
+                  <div className='px-2 py-0'>
+                    <i class='fa fa-trash pe-2'></i>
+                    Log Out
+                  </div>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
           </form>
         </nav>
       </nav>
